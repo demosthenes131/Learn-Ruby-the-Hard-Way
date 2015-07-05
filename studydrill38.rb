ac9b6829f7334c175d00c57c93bd6a5752dcb0c1
Nicolas Queen <nicolasqueen@gmail.com>
2015-07-05 01:39:28 +0000

    Adding Exercise 38 and 39

---
+++ studydrill38.rb
@@ -0,0 +1,5 @@
+breeds_of_dogs = ["Jack Russell Terrier", "Poodle", "German Shepherd", "Collie", "Pug"]
+
+puts "The best breed of dog is the #{breeds_of_dogs[0]}."
+
+puts breeds_of_dogs.join(' * ')
